eef48ea776eb1175396ab27b95d187b7b768bc60
Emily Saucedo <emilysaucedopires@gmail.com>
2023-07-13 22:37:41 -0300

    pseudocódigo forca

---
+++ forca.py
@@ -0,0 +1,16 @@
+#Selecionar uma palavra aleatória.
+#Esconder a palavra selecionada, substituindo as letras por traços ou asteriscos.
+#Definir o número de tentativas disponíveis.
+#Criar uma lista vazia para armazenar as letras já tentadas.
+#Enquanto houver tentativas restantes e a palavra escondida não tiver sido completamente revelada:
+#Exibir a forca de acordo com o número de tentativas restantes.
+#Exibir a palavra escondida com as letras descobertas.
+#Exibir as letras já tentadas.
+#Obter uma letra digitada pelo jogador.
+#Verificar se a letra já foi tentada anteriormente. Se sim, exibir uma mensagem informando que a letra já foi tentada e voltar para o início do loop.
+#Adicionar a letra à lista de letras tentadas.
+#Verificar se a letra está presente na palavra selecionada:
+#Se sim, exibir uma mensagem informando que a letra foi encontrada e revelar todas as ocorrências na palavra escondida.
+#Se não, exibir uma mensagem informando que a letra não foi encontrada e decrementar o número de tentativas.
+#Se o jogador tiver acertado todas as letras da palavra antes de ficar sem tentativas, exibir uma mensagem de vitória.
+#Caso contrário, exibir uma mensagem informando que o jogador perdeu e mostrar a palavra correta.
